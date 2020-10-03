47617c0c58271cc591ae15482b421cad52645d4e
Victor Jucosky <vjucosky@gmail.com>
2020-10-03 10:45:59 -0300

    Atualizado

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = ['*', ]
 
 # Application definition
 
+AUTH_USER_MODEL = 'user.User'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
