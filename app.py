c554cf5a24d169ff3dedd31b6f93d1385bb8107d
Julio Bandeira de Carvalho <juliobandeiradecarvalho@gmail.com>
2020-10-03 03:07:26 -0300

    app

--- app.py
+++ app.py
@@ -3,22 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-html = """
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>Nasa Hackathon</title>
-</head>
-<body>
-    <div>
-        <h1>hello, World!</h1>
-        <p>Principalmente você, Marte</p>
-    </div>
-</body>
-</html>
-"""
-
 @app.route('/')
 def index():
     return html
