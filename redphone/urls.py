1936b72e3f0e28e58e0279b6c366842821f8f6bc
julio bandeira de carvalho <juliobandeiradecarvalho@gmail.com>
2020-10-04 11:06:43 -0300

    paginas

--- redphone/urls.py
+++ redphone/urls.py
@@ -4,4 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('about', AboutView.as_view()),
+    path('crew', CrewView.as_view()),
+    path('chatbot', ChatbotView.as_view())
 ]
--- redphone/views.py
+++ redphone/views.py
@@ -5,3 +5,15 @@ from django.views import View
 class IndexView(View):
     def get(self, request):
         return render(request, 'redphone/index.html')
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'redphone/about.html')
+
+class CrewView(View):
+    def get(self, request):
+        return render(request, 'redphone/crew.html')
+
+class ChatbotView(View):
+    def get(self, request):
+        return render(request, 'redphone/chatbot.html')
