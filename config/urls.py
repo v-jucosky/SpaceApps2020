90998273c76c86b55f1b7fec235f70abfe8ddc3f
Victor Jucosky <vjucosky@gmail.com>
2020-10-03 23:19:36 -0300

    Versão inicial

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('redphone.urls')),
 ]
---
+++ redphone/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RedPhoneConfig(AppConfig):
+    name = 'redphone'
+    verbose_name = 'RedPhone'
